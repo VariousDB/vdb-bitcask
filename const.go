66c5e29e4951a949703e5cce9494f92d71753ada
zach <zachchou016@gmail.com>
2021-11-26 01:30:04 +0800

    add essential comment for const

--- const.go
+++ const.go
@@ -1,9 +1,8 @@
 package bitcask
 
 const (
-	ArchivedDataFile = "bitcask"
-	DataFileExt      = ".data"
-	IndexFile        = "index"
-	IndexTmpName     = "index-tmp"
-	MergeTmpFolder   = "merge"
+	DataFileExt    = ".data"     // 数据文件后缀
+	IndexFile      = "index"     // 索引文件名
+	IndexTmpName   = "index-tmp" // 临时索引文件名
+	MergeTmpFolder = "merge"     // 临时合并文件夹名
 )
